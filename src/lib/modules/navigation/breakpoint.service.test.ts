d130d759a923503da2f27c44358d1fca90de22ea
Maximilian Friedmann <max@smallstack.com>
2024-07-01 15:58:38 +0200

    Fix test

--- src/lib/modules/navigation/breakpoint.service.test.ts
+++ src/lib/modules/navigation/breakpoint.service.test.ts
@@ -4,16 +4,16 @@ import { breakpointService } from "./breakpoint.service.svelte.js";
 describe("BreakpointService", () => {
 	it("should match breakpoints", () => {
 		breakpointService.width = 350;
-		expect(breakpointService.matchesBreakpoints(["xs"])).toBe(true);
-		expect(breakpointService.matchesBreakpoints(["sm"])).toBe(false);
-		expect(breakpointService.matchesBreakpoints(["xs", "sm"])).toBe(true);
-		expect(breakpointService.matchesBreakpoints(["xs", "sm", "lg"])).toBe(true);
-		expect(breakpointService.matchesBreakpoints(["lg"])).toBe(false);
+		expect(breakpointService.matches(["xs"])).toBe(true);
+		expect(breakpointService.matches(["sm"])).toBe(false);
+		expect(breakpointService.matches(["xs", "sm"])).toBe(true);
+		expect(breakpointService.matches(["xs", "sm", "lg"])).toBe(true);
+		expect(breakpointService.matches(["lg"])).toBe(false);
 		breakpointService.width = 800;
-		expect(breakpointService.matchesBreakpoints(["xs"])).toBe(false);
-		expect(breakpointService.matchesBreakpoints(["sm"])).toBe(false);
-		expect(breakpointService.matchesBreakpoints(["xs", "sm"])).toBe(false);
-		expect(breakpointService.matchesBreakpoints(["xs", "sm", "md"])).toBe(true);
-		expect(breakpointService.matchesBreakpoints(["lg"])).toBe(false);
+		expect(breakpointService.matches(["xs"])).toBe(false);
+		expect(breakpointService.matches(["sm"])).toBe(false);
+		expect(breakpointService.matches(["xs", "sm"])).toBe(false);
+		expect(breakpointService.matches(["xs", "sm", "md"])).toBe(true);
+		expect(breakpointService.matches(["lg"])).toBe(false);
 	});
 });
