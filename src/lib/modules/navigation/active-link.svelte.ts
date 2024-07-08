cfcbd5630ac4034ec186b7c00417ee120c189b31
Maximilian Friedmann <max@smallstack.com>
2024-07-08 10:59:15 +0200

    Fix active link

--- src/lib/modules/navigation/active-link.svelte.ts
+++ src/lib/modules/navigation/active-link.svelte.ts
@@ -1,12 +1,16 @@
-import { afterNavigate } from "$app/navigation";
-import type { AfterNavigate } from "@sveltejs/kit";
+import { browser } from "$app/environment";
+import { beforeNavigate } from "$app/navigation";
+import type { BeforeNavigate } from "@sveltejs/kit";
 
 export function isActiveLink(node: HTMLAnchorElement, activeClass: string) {
-	afterNavigate((navigation: AfterNavigate) => {
+	function checkHref(pathname: string) {
 		if (node.href) {
-			if (new URL(node.href).pathname === navigation.to.url.pathname)
-				node.classList.add(activeClass);
+			if (new URL(node.href).pathname === pathname) node.classList.add(activeClass);
 			else node.classList.remove(activeClass);
 		}
+	}
+	beforeNavigate((navigation: BeforeNavigate) => {
+		checkHref(navigation.to.url.pathname);
 	});
+	if (browser) checkHref(location.pathname);
 }
