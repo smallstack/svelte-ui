53ed184fe5bdcea81d159b45f93620e774ad1fa3
Max Friedmann <max@smallstack.com>
2024-11-21 17:22:44 +0100

    Fix exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,8 @@
 export * from "./form-inputs";
 export * from "./layout";
 export * from "./modules/modals";
-export * from "./modules/navigation/index";
+export * from "./modules/navigation";
+export * from "./modules/utils";
 export * from "./overlay";
 export * from "./stores/i18n.store";
 
