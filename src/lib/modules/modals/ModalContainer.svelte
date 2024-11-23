9999c572d4d49ef542591662e5014f37610f2c24
Max Friedmann <max@smallstack.com>
2024-11-23 13:35:36 +0100

    Fix modal arrow

--- src/lib/modules/modals/ModalContainer.svelte
+++ src/lib/modules/modals/ModalContainer.svelte
@@ -59,7 +59,11 @@
 				{#if previousModal}
 					<div class="tooltip tooltip-right" data-tip="Zurück zu {previousModal.options.title}">
 						<button class="btn btn-xs bg-transparent btn-square" onclick={() => modalStack.pop()}>
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 448 512"
+								fill="currentColor"
+								class="w-4 h-4"
 								><path
 									d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"
 								/></svg
