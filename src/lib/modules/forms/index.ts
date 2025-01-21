6e4660fa65c0593ae5d08434530de1593f4cee22
Maximilian Friedmann <max@smallstack.com>
2025-01-21 09:32:39 +0100

    Signature Pad Form Input Fixes #177

--- src/lib/modules/forms/index.ts
+++ src/lib/modules/forms/index.ts
@@ -1,3 +1,3 @@
 export { default as DateTimeRangeInput } from "./form-inputs/DateTimeRangeInput.svelte";
 export { default as StringInput } from "./form-inputs/StringInput.svelte";
-
+export { default as SignaturePadInput } from "./form-inputs/signature-pad/SignaturePadInput.svelte";
