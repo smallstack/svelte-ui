4ffc10c1ebb3c64089f9551fc4644e74098aebfa
Maximilian Friedmann <max@smallstack.com>
2025-01-31 16:15:26 +0100

    Export missing Modal component

--- src/lib/modules/modals/index.ts
+++ src/lib/modules/modals/index.ts
@@ -1,4 +1,5 @@
 export * from "./modal.service.svelte";
+export { default as Modal } from "./Modal.svelte";
 export { default as ModalContainer } from "./ModalContainer.svelte";
 export { default as SimpleModal } from "./SimpleModal.svelte";
 
